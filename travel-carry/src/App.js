a50b6f8c45d6e055386d054045477eecc0ec61fa
DIALLO-Hawa <hawadiallo29a@gmail.com>
2024-12-21 15:33:12 +0100

    protection des routes

--- travel-carry/src/App.js
+++ travel-carry/src/App.js
@@ -1,29 +1,71 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Header from "./Header";
 import HomePage from "./HomePage";
 import Login from "./auth/Login";
 import SignUp from "./auth/SignUp";
-import NotationsPage from "./NotationsPage"; // Import de la page Notations
-import ColisDetails from "./colis/ColisDetails"; // Import du composant ColisDetails
-import AnnonceList from "./AnnonceList"; // Ajout pour conserver la fonctionnalité d'AnnonceList
-import { UserProvider } from "./context/UserContext";
+import NotationsPage from "./NotationsPage";
+import ColisDetails from "./colis/ColisDetails";
+import AnnonceList from "./AnnonceList";
 import CreateAnnonce from "./CreateAnnonce";
+import { UserProvider } from "./context/UserContext";
 
 function App() {
+    // Vérifie si l'utilisateur est connecté
+    const isAuthenticated = !!localStorage.getItem("token");
+
     return (
         <UserProvider>
             <Router>
                 <Header />
                 <div className="container">
                     <Routes>
+                        {/* Routes publiques */}
                         <Route path="/" element={<HomePage />} />
                         <Route path="/login" element={<Login />} />
                         <Route path="/register" element={<SignUp />} />
-                        <Route path="/notations" element={<NotationsPage />} /> {/* Route Notations */}
-                        <Route path="/colis/details" element={<ColisDetails />} /> {/* Nouvelle route */}
-                        <Route path="/annonces" element={<AnnonceList />} /> {/* Intégration d'AnnonceList */}
-                        <Route path="/create-annonce" element={<CreateAnnonce />} />
+
+                        {/* Routes protégées */}
+                        <Route
+                            path="/notations"
+                            element={
+                                isAuthenticated ? (
+                                    <NotationsPage />
+                                ) : (
+                                    <Navigate to="/login" replace />
+                                )
+                            }
+                        />
+                        <Route
+                            path="/colis/details"
+                            element={
+                                isAuthenticated ? (
+                                    <ColisDetails />
+                                ) : (
+                                    <Navigate to="/login" replace />
+                                )
+                            }
+                        />
+                        <Route
+                            path="/annonces"
+                            element={
+                                isAuthenticated ? (
+                                    <AnnonceList />
+                                ) : (
+                                    <Navigate to="/login" replace />
+                                )
+                            }
+                        />
+                        <Route
+                            path="/create-annonce"
+                            element={
+                                isAuthenticated ? (
+                                    <CreateAnnonce />
+                                ) : (
+                                    <Navigate to="/login" replace />
+                                )
+                            }
+                        />
                     </Routes>
                 </div>
             </Router>
